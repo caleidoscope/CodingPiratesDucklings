8b9189beb7cfc01e6a024eab860277e2696e53b5
karsten zakarias <extkazak@pandora.net>
2025-09-18 14:55:31 +0200

    Lesson 1: Placing the Duck (Variables)

--- sketch.js
+++ sketch.js
@@ -1,9 +1,17 @@
+let duckImg;
+let duckX = 200;
+let duckY = 200;
+
+function preload() {
+  duckImg = loadImage('duck.png');
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
-  textSize(32);
-  text('Hello Pirates!', 10, 30);
+  // Draw the duck at the position defined by variables
+  image(duckImg, duckX, duckY, 100, 100);
 }
